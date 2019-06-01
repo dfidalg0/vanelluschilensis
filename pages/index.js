10110f4fdcbcf1cd8fba9e1a532c4683330b43ab
nickolasbmm <nickolasbmm44@gmail.com>
2019-06-01 19:47:57 -0300

    Borala

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import "react";
 import Materias from "../components/materias.js";
 import Faltas from "../components/faltas.js";
-import Calendario from "../components/calendario.js";
+import Agenda from "../components/agenda.js";
 import Desistencia from "../components/desistencia.js";
 
 export default function Index() {
     return (
         <div>
-            <Materias/>
-            <Faltas/>
-            <Calendario/>
-            <Desistencia/>
+            <Agenda/>
         </div>
     )
 }
