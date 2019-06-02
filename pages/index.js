3679f06118841c8f950742f5b98a5c5abc806d77
nickolasbmm <nickolasbmm44@gmail.com>
2019-06-01 21:55:28 -0300

    FullCalendar funcionando

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import "react";
-import Materias from "../components/materias.js";
-import Faltas from "../components/faltas.js";
-import Agenda from "../components/agenda.js";
-import Desistencia from "../components/desistencia.js";
+import FullCalendar from '../components/fullcalendar'
 
 export default function Index() {
     return (
         <div>
-            <Agenda/>
+        <FullCalendar defaultView='dayGridMonth' />
         </div>
     )
 }
